097179f5b73022269f4c3832c41a83b01b015ee6
Eduardo B Damacena <eduardobusato51@gmail.com>
2023-03-29 23:16:56 -0300

    feat: check local exists before list objects

--- api/services/local_service.py
+++ api/services/local_service.py
@@ -1,6 +1,10 @@
 from api.models import Local
 
 
+def listar_local_id(local_id):
+    return Local.objects.get(id=local_id)
+
+
 def listar_local_usuario(usuario_id):
     return Local.objects.filter(usuario_id=usuario_id).first()
 
--- api/views/local_objects_view.py
+++ api/views/local_objects_view.py
@@ -1,13 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status as status_http
 from api.serializers.local_objects_serializer import LocalObjectsSerializer
-from api.services import objeto_service
+from api.services import objeto_service, local_service
 
 
 class LocalObjectsView(APIView):
 
     def get(self, request, local_id, format=None):
+        try:
+            local = local_service.listar_local_id(local_id)
+        except ObjectDoesNotExist:
+            return Response(data={"message": "Local não encontrado"}, status=status_http.HTTP_404_NOT_FOUND)
         objects = objeto_service.listar_objetos_local_nao_entregue(local_id)
         local_object_serializer = LocalObjectsSerializer(objects, many=True, context={"request": request})
         return Response(data=local_object_serializer.data, status=status_http.HTTP_200_OK)
